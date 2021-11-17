5f8078569b334882d9204ab77ee37e3d2f192a0e
lvdat <datb2012191@student.ctu.edu.vn>
2021-11-17 09:05:00 +0700

    them bai tap cai dat ngan xep

---
+++ STACK/Cài đặt ngăn xếp/readme.md
@@ -0,0 +1,13 @@
+# Cài đặt ngăn xếp
+Các bài thực hành trong phần Cài đặt ngăn xếp
+
+# Khai báo và các hàm sử dụng
+- Xem trong file [stack.c](stack.c):
+  - [giatriDinh](stack.c#L17)
+  - [ktRong](stack.c#L20)
+  - [ktDay](stack.c#L23)
+  - [xoa](stack.c#L27)
+  - [khoitao](stack.c#L32)
+  - [hienthi](stack.c#L35)
+  - [them](stack.c#L41)
+  - [doiNhiPhan](stack.c#L47)
---
+++ STACK/Cài đặt ngăn xếp/stack.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define M 100 // so luong phan tu toi da
+#define SoPhanTu 100
+
+typedef int ElementType;
+// Tất cả hàm bên dưới đều có dpt O(1)
+
+typedef struct {
+    ElementType DuLieu[M];
+    int Dinh; // vị trí đỉnh stack
+} Stack;
+
+typedef Stack NganXep;
+
+/* ===== */
+
+int giatriDinh(NganXep S){
+    return S.DuLieu[S.Dinh];
+}
+int ktRong(NganXep S){
+    return S.Dinh == 100;
+}
+int ktDay(NganXep S){
+    return S.Dinh == 0;
+}
+
+void xoa(NganXep *pS){
+    if(!ktRong(*pS)){
+        pS->Dinh++;
+    }
+}
+void khoitao(NganXep *pS){
+    pS->Dinh = 100;
+}
+void hienthi(NganXep *pS){
+    while(!ktRong(*pS)){
+        printf("%d ", giatriDinh(*pS));
+        xoa(pS);
+    }
+}
+void them(int x, NganXep *pS){
+    if(pS->Dinh != 0){
+        pS->Dinh--;
+        pS->DuLieu[pS->Dinh] = x;
+    }
+}
+void doiNhiPhan(int n, NganXep *pS){
+    khoitao(pS);
+    int x = n;
+    do {
+        them(x % 2, pS);
+        x /= 2;
+    } while(x > 0);
+}
+
